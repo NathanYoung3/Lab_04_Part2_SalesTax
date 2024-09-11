aa128457cad7706e94cada1e18c0326632716dc6
Natedawg2077 <NatedawgGDEV@gmail.com>
2024-09-11 08:54:23 -0400

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //variables
+        Scanner scan = new Scanner(System.in);
+        double initialValue;
+        double salesTax = 0.05;
+        double tax;
+        double total;
+
+        System.out.println("Enter the price of a purchase.");
+        initialValue = scan.nextDouble();
+        tax = initialValue * salesTax;
+        total = initialValue + tax;
+
+        System.out.println("For a purchase of $" + initialValue + " with a sales tax of " + salesTax * 100 + "%," +
+                " the sales tax will be $" + tax + " bringing the total to $" + total + ".");
     }
 }
